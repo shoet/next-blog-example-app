ce60ab7e353a74bc00927f460876d53dd84ea56b
shoet <32211937+shoet@users.noreply.github.com>
2023-08-05 17:00:58 +0900

    GlobalStyle修正

--- src/components/templates/GlobalStyle/index.tsx
+++ src/components/templates/GlobalStyle/index.tsx
@@ -6,12 +6,16 @@ const GlobalStyle = createGlobalStyle`
   html {
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
+  * {
+    box-sizing: border-box;
+  }
   a {
     text-decoration: none;
     color: black;
+    cursor: pointer;
   }
-  * {
-    box-sizing: border-box;
+  ol, ul {
+    list-style: none;
   }
 `
 
