1100d3e4f4df24d1548a4078f94de55db13d1500
shoet <32211937+shoet@users.noreply.github.com>
2023-08-05 09:50:35 +0900

    Revert "Revert "GlobalStyle追加""
    This reverts commit 9b1fff7e5c308542f2f19b6c259308e3af7c1556.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-// import '@/styles/globals.css'
 import Layout from '@/components/templates/Layout'
 import type { AppProps } from 'next/app'
+import GlobalStyle from '@/components/templates/GlobalStyle'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
