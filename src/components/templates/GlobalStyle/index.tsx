2b0ecdb29abedc0ed7d4266c0eb9927fa622ae26
shoet <32211937+shoet@users.noreply.github.com>
2023-08-05 15:21:29 +0900

    フォント追加

--- src/components/templates/GlobalStyle/index.tsx
+++ src/components/templates/GlobalStyle/index.tsx
@@ -3,6 +3,9 @@ import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
   a {
     text-decoration: none;
     color: black;
