8486ee9b17351172e67b9f9bb0b4de2f7bd1394c
shoet <32211937+shoet@users.noreply.github.com>
2023-08-06 17:22:01 +0900

    Hero - レイアウト修正

--- src/components/organisms/Hero/index.tsx
+++ src/components/organisms/Hero/index.tsx
@@ -1,5 +1,7 @@
 import Text from '@/components/atoms/Text'
 import Box from '@/components/layout/Box'
+import Flex from '@/components/layout/Flex'
+import Image from 'next/image'
 
 type HeroProps = {
   title: string
@@ -11,19 +13,35 @@ const Hero = (props: HeroProps) => {
   const { title, subTitle, imageOn } = props
   return (
     <>
-      <Box marginBottom={1}>
-        <Text
-          fontSize={{ base: '36px', sm: '72px', md: '160px' }}
-          fontWeight="900"
-          letterSpacing="large"
-        >
-          {title}
-        </Text>
-      </Box>
-      <Box>
-        <Text variant="medium">{subTitle}</Text>
-      </Box>
-      {imageOn && <figure>画像</figure>}
+      <Flex
+        flexDirection={{ base: 'column', lg: 'row' }}
+        justifyContent={{ lg: 'space-between' }}
+        alignItems={{ base: 'center' }}
+      >
+        <Box marginBottom={{ base: 3, xl: 0 }}>
+          <Box marginBottom={1}>
+            <Text
+              fontSize={{ base: '36px', sm: '72px', md: '160px' }}
+              fontWeight="900"
+              letterSpacing="large"
+            >
+              {title}
+            </Text>
+          </Box>
+          <Flex justifyContent={{ base: 'center', lg: 'start' }}>
+            <Text variant="medium">{subTitle}</Text>
+          </Flex>
+        </Box>
+        {imageOn && (
+          <Image
+            src="/myImage.jpg"
+            alt="centerImage"
+            style={{ objectFit: 'contain' }}
+            width={300}
+            height={150}
+          />
+        )}
+      </Flex>
     </>
   )
 }
