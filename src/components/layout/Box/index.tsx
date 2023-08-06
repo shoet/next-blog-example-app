e49ba0a926e836bd439dfb8d6877534160dd9e3e
shoet <32211937+shoet@users.noreply.github.com>
2023-08-06 11:51:59 +0900

    Responsive追加(Box)

--- src/components/layout/Box/index.tsx
+++ src/components/layout/Box/index.tsx
@@ -1,26 +1,26 @@
-import { Color, Space, toThemeValue } from '@/utils/styles'
+import { Color, Responsive, Space, toResponsiveValue } from '@/utils/styles'
 import { styled } from 'styled-components'
 import { theme } from '@/themes'
 
 type BoxProps = {
-  color?: Color
-  backgroundColor?: Color
-  margin?: Space
-  marginTop?: Space
-  marginRight?: Space
-  marginBottom?: Space
-  marginLeft?: Space
-  padding?: Space
-  paddingTop?: Space
-  paddingRight?: Space
-  paddingBottom?: Space
-  paddingLeft?: Space
-  width?: string
-  maxWidth?: string
-  minWidth?: string
-  height?: string
-  maxHeight?: string
-  minHeight?: string
+  color?: Responsive<Color>
+  backgroundColor?: Responsive<Color>
+  margin?: Responsive<Space>
+  marginTop?: Responsive<Space>
+  marginRight?: Responsive<Space>
+  marginBottom?: Responsive<Space>
+  marginLeft?: Responsive<Space>
+  padding?: Responsive<Space>
+  paddingTop?: Responsive<Space>
+  paddingRight?: Responsive<Space>
+  paddingBottom?: Responsive<Space>
+  paddingLeft?: Responsive<Space>
+  width?: Responsive<string>
+  maxWidth?: Responsive<string>
+  minWidth?: Responsive<string>
+  height?: Responsive<string>
+  maxHeight?: Responsive<string>
+  minHeight?: Responsive<string>
 }
 
 const Box = styled.div.withConfig({
@@ -41,32 +41,26 @@ const Box = styled.div.withConfig({
       'minHeight',
     ].includes(props),
 })<BoxProps>`
-  color: ${(props) => toThemeValue('color', props.color, theme)};
-  background-color: ${(props) =>
-    toThemeValue('background-color', props.backgroundColor, theme)};
-  padding: ${(props) => toThemeValue('padding', props.padding, theme)};
-  padding-top: ${(props) =>
-    toThemeValue('padding-top', props.paddingTop, theme)};
-  padding-right: ${(props) =>
-    toThemeValue('padding-right', props.paddingRight, theme)};
-  padding-bottom: ${(props) =>
-    toThemeValue('padding-bottom', props.paddingBottom, theme)};
-  padding-left: ${(props) =>
-    toThemeValue('padding-left', props.paddingLeft, theme)};
-  margin: ${(props) => toThemeValue('margin', props.margin, theme)};
-  margin-top: ${(props) => toThemeValue('margin-top', props.marginTop, theme)};
-  margin-right: ${(props) =>
-    toThemeValue('margin-right', props.marginRight, theme)};
-  margin-bottom: ${(props) =>
-    toThemeValue('margin-bottom', props.marginBottom, theme)};
-  margin-left: ${(props) =>
-    toThemeValue('margin-left', props.marginLeft, theme)};
-  width: ${(props) => toThemeValue('width', props.width, theme)};
-  max-width: ${(props) => toThemeValue('max-width', props.maxWidth, theme)};
-  min-width: ${(props) => toThemeValue('min-width', props.minWidth, theme)};
-  height: ${(props) => toThemeValue('height', props.height, theme)};
-  max-height: ${(props) => toThemeValue('max-height', props.maxWidth, theme)};
-  min-height: ${(props) => toThemeValue('min-height', props.minHeight, theme)};
+
+  ${(props) => toResponsiveValue('color', props.color, theme)}
+  ${(props) =>
+    toResponsiveValue('background-color', props.backgroundColor, theme)}
+  ${(props) => toResponsiveValue('margin', props.margin, theme)}
+  ${(props) => toResponsiveValue('margin-top', props.marginTop, theme)}
+  ${(props) => toResponsiveValue('margin-right', props.marginRight, theme)}
+  ${(props) => toResponsiveValue('margin-bottom', props.marginBottom, theme)}
+  ${(props) => toResponsiveValue('margin-left', props.marginLeft, theme)}
+  ${(props) => toResponsiveValue('padding', props.padding, theme)}
+  ${(props) => toResponsiveValue('padding-top', props.paddingTop, theme)}
+  ${(props) => toResponsiveValue('padding-right', props.paddingRight, theme)}
+  ${(props) => toResponsiveValue('padding-bottom', props.paddingBottom, theme)}
+  ${(props) => toResponsiveValue('padding-left', props.paddingLeft, theme)}
+  ${(props) => toResponsiveValue('width', props.width, theme)}
+  ${(props) => toResponsiveValue('max-width', props.maxWidth, theme)}
+  ${(props) => toResponsiveValue('min-width', props.minWidth, theme)}
+  ${(props) => toResponsiveValue('height', props.height, theme)}
+  ${(props) => toResponsiveValue('max-height', props.maxHeight, theme)}
+  ${(props) => toResponsiveValue('min-height', props.minHeight, theme)}
 `
 
 export default Box
