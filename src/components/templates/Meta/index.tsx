3dd6dfda5869373104c5bbc0773c65120893e5b9
shoet <32211937+shoet@users.noreply.github.com>
2023-08-07 22:37:27 +0900

    Meta追加

--- src/components/templates/Meta/index.tsx
+++ src/components/templates/Meta/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { siteMeta } from '@/lib/constants'
 import { useRouter } from 'next/router'
+import { StaticImageData } from 'next/image'
+import siteImg from 'images/heroImage.jpg'
 
 const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
   siteMeta
@@ -8,15 +10,24 @@ const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
 type MetaProps = {
   pageTitle?: string
   pageDesc?: string
+  pageImg?: string
+  pageImgW?: string
+  pageImgH?: string
 }
 
 const Meta = (props: MetaProps) => {
-  const { pageTitle, pageDesc } = props
+  const { pageTitle, pageDesc, pageImg, pageImgW, pageImgH } = props
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
   const desc = pageDesc ?? siteDesc
 
   const router = useRouter()
   const url = `${siteUrl}${router.asPath}`
+
+  const img = pageImg || siteImg.src
+  const imgW = pageImgW || `${siteImg.width}`
+  const imgH = pageImgH || `${siteImg.height}`
+  const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
+
   return (
     <Head>
       <title>{title}</title>
@@ -34,6 +45,11 @@ const Meta = (props: MetaProps) => {
 
       <link rel="icon" href={siteIcon} />
       <link rel="apple-touch-icon" href={siteIcon} />
+
+      <meta property="og:image" content={imgUrl} />
+      <meta property="og:image:width" content={imgW} />
+      <meta property="og:image:height" content={imgH} />
+      <meta property="twitter:card" content="summary_large_image" />
     </Head>
   )
 }
